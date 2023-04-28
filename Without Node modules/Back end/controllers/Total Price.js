ee0879ac42f51b83abb706dee3b1b27f798d4a87
RajatBudhathoki1234 <Budhathokirajat31@gmail.com>
2023-04-29 00:46:13 +0545

    some minor changes

--- Without Node modules/Back end/controllers/Total Price.js
+++ Without Node modules/Back end/controllers/Total Price.js
@@ -1,20 +1,51 @@
-const Banquet = require("../models/banquet");
+const banquetModel = require("../models/banquet");
 
-const calculateTotalPrice = async (req, res) => {
+const menuSchema = require("../models/menu");
+
+//Importing bcrypt to hash the password.
+const bcrypt = require("bcryptjs");
+
+//Importing jwt to create a token.
+const jwt = require("jsonwebtoken");
+
+const createMenu = async (req, res) => {
   try {
-    const { banquet_price, guest } = req.body;
-    const total_price = Number(banquet_price) * guest.length;
-
-    // Do something with the total_price value, e.g. save it to the database or return it to the client
-    // ...
-
-    res.status(200).json({ total_price });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: "Error calculating total price" });
+    //Destructing array.
+    const [breakfast, dinner, desert, price] = req.body.menu;
+    const { token } = req.params;
+    //Decoding the token with secret key and token.
+    let decoded = await jwt.verify(token, "jwtsecret");
+    console.log(decoded);
+    if (breakfast && dinner && desert && price) {
+      //createing banquet.
+      const createBanquet = await banquetModel.create({
+        ...decoded,
+      });
+      //Creating menu.
+      await menuSchema.create({
+        userId: req.signedCookies.userId,
+        banquetId: createBanquet._id,
+        breakfast,
+        dinner,
+        desert,
+        price,
+      });
+      return res.status(200).json("Sucess");
+    }
+    res.json("Unsucessfull");
+  } catch (error) {
+    console.log(error);
   }
 };
 
-module.exports = {
-  calculateTotalPrice,
+const getMenu = async (req, res) => {
+  try {
+    const { banquetId } = req.params;
+    const menu = await menuSchema.findOne({ banquetId: banquetId });
+    res.status(200).json(menu);
+  } catch (error) {
+    console.log(error);
+  }
 };
+
+module.exports = { createMenu, getMenu };
